fe91884c28970a9e7b5aee29b418e94685b32907
Colin Pfingstl <colin.pfingstl@hotmail.ch>
2022-02-03 00:39:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# This script is for production only. It sets up the production environment
+
 echo "Copy .env file into dist"
 cp ../../../.env .
+
+MEDIA_DIR="../../../media"
+if [ -d "$MEDIA_DIR"]; then
+	echo "Create media folder"
+fi
+
+echo "Link media folder"
+ln -s $MEDIA_DIR media
