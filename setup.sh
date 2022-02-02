29e0b7be80050510456525540d8593c0ddb49227
Colin Pfingstl <colin.pfingstl@hotmail.ch>
2022-02-03 00:58:56 +0100

    Stop and start service in CI

--- setup.sh
+++ setup.sh
@@ -6,9 +6,10 @@ echo "Copy .env file into dist"
 cp ../../../.env .
 
 MEDIA_DIR="../../../media"
-if [ -d "$MEDIA_DIR"]; then
+if [ ! -d "$MEDIA_DIR" ]; then
 	echo "Create media folder"
+	mkdir $MEDIA_DIR
 fi
 
 echo "Link media folder"
-ln -s $MEDIA_DIR media
+ln -s -f $MEDIA_DIR ./media
